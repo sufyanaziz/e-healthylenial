0470d6612bdc7a96ede6ca1b185535e17ec97091
sufyanaziz <achmadsufyan98@gmail.com>
2020-11-19 00:03:14 +0700

    update instance axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/healthylenial/api",
+  baseURL: "https://app-healthylenial-api.herokuapp.com/healthylenial/api",
 });
 
 export default instance;
